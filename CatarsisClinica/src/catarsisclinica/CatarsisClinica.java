f5887bf84771fa362077af46214f433753eb6115
paulbt94 <paulsb.94@gmail.com>
2019-04-19 00:04:03 -0600

    Menu inicio

---
+++ CatarsisClinica/src/catarsisclinica/CatarsisClinica.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catarsisclinica;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pauls
+ */
+public class CatarsisClinica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int opcion = 1;
+        Scanner scan = new Scanner(System.in);
+        
+        
+        //Inicializar clase
+        //Saludo de bienvenida
+        System.out.println("Bienvenido al Sistema de gestion de expedientes electronicos de la Clinica Catarsis.");
+        System.out.println("Por favor digite el numero de una de las siguientes opciones:");
+        
+        //Menu principal de usuario
+        while(opcion != 0) {
+            System.out.println("1. Citas");
+            System.out.println("2. Expedientes");
+            System.out.println("3. Reportes");
+            System.out.println("0. Salir");
+            opcion = scan.nextInt();
+            //Continuar el if con las opciones de la clase
+        }
+        
+    }
+    
+}
